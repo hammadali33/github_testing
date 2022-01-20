eb309fad6f2a2f6711530963e0540a0e24db6a4a
Mr General <mrgeneral38@gmail.com>
2022-01-21 03:57:38 +0500

    2

---
+++ lib/tiktok_ui/tiktok_ui_main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TikTokUiMain extends StatelessWidget {
+  const TikTokUiMain({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
