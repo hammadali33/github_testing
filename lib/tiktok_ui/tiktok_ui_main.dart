fe84f4387d4d9e9ca6d6492f40feee53e5caf7fa
Mr General <mrgeneral38@gmail.com>
2022-01-21 05:28:06 +0500

    3

--- lib/tiktok_ui/tiktok_ui_main.dart
+++ lib/tiktok_ui/tiktok_ui_main.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class TikTokUiMain extends StatelessWidget {
   const TikTokUiMain({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: PageView(
+        scrollDirection: Axis.vertical,
+        children: [
+          pages(Colors.amber),
+          pages(Colors.black),
+          pages(Colors.green),
+          pages(Colors.orange),
+          Text("saasassasas"),
+          Center(
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  Container(),
+                  Container(),
+                  Text(""),
+                  Text(""),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container pages(Color c) {
+    return Container(height: double.infinity, width: double.infinity, color: c);
   }
 }
