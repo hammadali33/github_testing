76901503cf60ca91b701761b4ad40e06aff6f754
Mr General <mrgeneral38@gmail.com>
2022-01-24 23:35:46 +0500

    tiktok_ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:github_testing/assignments/bike_shop.dart';
-import 'package:github_testing/tiktok_ui/tiktok_ui_main.dart';
 import 'package:sizer/sizer.dart';
 
 void main() {
